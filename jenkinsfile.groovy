20d0007b9040e9383e318d6c48d748d609c32b1a
skruvi <sagikruvi@gmail.com>
2019-06-19 17:37:42 +0300

    Update Build command

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node any {
     cleanWs()
     stage('Build') {
-        sh label: '', script: 'docker build -f ./Dockerfile . -t skruvi/worldofgames:latest'
+        sh 'docker build -f ./Dockerfile . -t skruvi/worldofgames:latest'
     }
 }
