65a924fd08cfbe438334b9018e9b9ba537760047
Sagi Kruvi <sagi.kruvi@verint.com>
2019-06-20 15:56:32 +0300

    Add jenkins new file

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,35 @@
-node any {
-    stage('Build') {
-        echo 'Docker image build'
-        bat label: '', script: 'docker build -f ./Dockerfile . -t skruvi/worldofgames:latest'
+pipeline {
+    agent any
+    stages {
+        stage('pull') {
+            steps{
+            git 'https://github.com/skruvi/WorldOfGames.git'
+         }
+        }
+        stage('Build') {
+            steps{
+            echo 'Docker image build'
+            bat 'docker build -f ./Dockerfile . -t skruvi/worldofgames:latest'
+         }
+        }
+        stage('Run') {
+            steps{
+                echo 'Start docker container'
+                bat 'docker compose up -d'
+            }
+        }
+        stage('Test') {
+            steps{
+                echo 'Testing...'
+                bat (script: 'python e2e.py')
+            }
+        }
+        stage('Close and publish'){
+            steps {
+                echo 'Stop the container'
+                bat 'docker compose down'
+                bat 'docker push skruvi/worldofgames:latest'
+            }
+        }
+        }
     }
-}
